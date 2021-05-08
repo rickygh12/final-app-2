6424f726ca1b17533cd9ce5fade1f5ab70dff2e7
rickygh12 <81440077+rickygh12@users.noreply.github.com>
2021-05-08 15:17:29 -0500

    rg

--- final app 2/ViewController.swift
+++ final app 2/ViewController.swift
@@ -14,6 +14,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnmoose(_ sender: UIButton) {
+        if (btnrecipe.isHidden)
+        {self.btnrecipe.isHidden =
+            false
+        }else{
+            self.btnrecipe.isHidden
+            =
+            true
+        }
+    }
+    @IBOutlet weak var btnrecipe: UIButton!
 
+    
+    @IBOutlet weak var recipe4: UIButton!
+    
+    @IBAction func brownie1(_ sender: UIButton) {
+        if (recipe4.isHidden)
+        {self.recipe4.isHidden =
+            false
+        }else{
+            self.recipe4.isHidden
+            =
+            true
+        }
+    }
+    
+    
+    
 }
-
