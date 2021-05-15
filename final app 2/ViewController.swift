fedd6012735978e186a4f08fe3b6c9270e69d46e
rickygh12 <81440077+rickygh12@users.noreply.github.com>
2021-05-15 11:42:44 -0500

    rg
    finalllllllll

--- final app 2/ViewController.swift
+++ final app 2/ViewController.swift
@@ -40,6 +40,59 @@ class ViewController: UIViewController {
         }
     }
     
+   
+    @IBOutlet weak var Bananarecipe: UIButton!
+    
+    @IBAction func BtnBanana(_ sender: UIButton) {
+        if (Bananarecipe.isHidden)
+        {self.Bananarecipe.isHidden =
+            false
+        }else{
+         self.Bananarecipe.isHidden
+        =
+        true
+    
+    
     
     
 }
+}
+  
+    
+    
+    
+    @IBOutlet weak var brownierecipe: UIButton!
+    @IBAction func browniepic(_ sender: UIButton) {
+        if (brownierecipe.isHidden)
+        {self.brownierecipe.isHidden =
+            false
+        }else{
+            self.brownierecipe.isHidden
+            =
+            true
+    }
+}
+    
+    @IBAction func emailmessage(_ sender: UIButton) {
+        
+        message()
+        
+        
+    }
+    
+    func message(){
+                let alert = UIAlertController(title: "you will be sent a confirmation email shortly.", message: "Thank you!.", preferredStyle: .alert)
+
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+
+                NSLog("The \"OK\" alert occured.")
+
+                }))
+
+                self.present(alert, animated: true, completion: nil)
+
+            }
+
+    }
+
+
